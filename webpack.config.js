91a5047ccffdd42bdb0c3c1e9aaf0b90c67b111f
Katlyn Tucker <studioquirk@yahoo.com>
2018-05-29 16:53:11 -0700

    building webpack config - added cleanWebpackPlugin and build devServer

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './build'
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(`${path}/bundle.*.js`),
+  ],
+};
