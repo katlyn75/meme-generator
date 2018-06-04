d4c6e072abbf1630c8427f08af5c558013201e77
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-03 21:47:41 -0700

    tweaking footer, header, labels for css

--- src/components/App.js
+++ src/components/App.js
@@ -10,21 +10,14 @@ export default class App extends Component {
     super();
 
     this.state = {
-      header: 'I don/t always drink tea',
+      header: 'I don\'t always drink tea',
       footer: 'because it tastes terrible.',
       image:'http://i0.kym-cdn.com/entries/icons/mobile/000/015/878/thatsnoneofmy.jpg',
       color: '#000000' 
     };
   }
 
-
-  handleBackground({ target }) {
-    this.setState({
-      background: target.value
-    });
-  }
-
-  handleNameChange({ target }) {
+  handleTextChange({ target }) {
     this.setState({ name: target.value });
   } 
 
@@ -65,27 +58,34 @@ export default class App extends Component {
 
     return (
       <main>
-        <div className="meme">
+        <div className="meme-maker">
           <header>
             <h1>
-            Meme-Maker
+              Meme-Maker
             </h1>
-          </header>
-          <section className="search-bars">
             <label>
-              Image File Upload:
-              <input name="file" onChange={this.handleUpload} placeholder="File"/>
-              Image URL:
-              <input name="url" onChange={this.handleBackground} placeholder="URL"/>
+              File Upload:
+              <input type="file" onChange={event => this.handleUpload(event)} placeholder="File"/>&nbsp;
+              Image URL: 
+              <input type="url" value={image} onChange={event => this.handleImgSrc(event)} placeholder="URL"/>&nbsp;
               Meme Text:
-              <input onChange={event => this.handleImageSrc(event)} placeholder="Meme Text"/>
+              <input type="text" onChange={event => this.handleImageSrc(event)}placeholder="Meme Text"/>&nbsp;
             </label> 
-          </section>
-          <fieldset>
-            <div className="image" ref={node => this.imageExport = node}>
+          </header>
+          <div className="image" ref={node => this.ImageExport = node}>
+            <section className="meme-header">
+              {header}
               <img src={image}/>
-            </div>
-          </fieldset>
+            </section>
+            <section className="meme-footer">
+              {footer}
+            </section>
+            <section className="button">
+              <button onClick={() => this.handleExport()}>
+              Export Meme
+              </button>
+            </section>
+          </div>
           <footer>
             Meme Your Mom 2018
           </footer>
